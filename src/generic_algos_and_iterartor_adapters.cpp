e302d53f7fa89feebd9e38e2645ddbb460833e2e
Vivek Sood <vsood@umd.edu>
2022-07-20 00:21:18 -0700

    generic algos and iterator adapters v2

--- src/generic_algos_and_iterartor_adapters.cpp
+++ src/generic_algos_and_iterartor_adapters.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
-int main() {
-    std::string str = "Hello World";
-    std::string::iterator begin = str.begin();
-    std::string::iterator end = str.end();
-
-    std::string::iterator out = str.begin() + 5;
-
-    while(begin != end) {
-        *out++ = *begin++;
+void print_seq(const auto& obj) {
+    std::cout << "[";
+    for(int i = 0; i != obj.size(); ++i) {
+        if(i != obj.size() - 1) std::cout << obj[i] << ", ";
+        else std::cout << obj[i];
     }
+    std::cout << "]" << std::endl;
+}
+
+int main()
+{   
+    std::vector<int> vec1 = {12,3,4,5};
+    std::vector<int> vec2;
+    copy(vec1.begin(), vec1.end(), back_inserter(vec2));
+    
+    print_seq(vec2);
+
     return 0;
 }
